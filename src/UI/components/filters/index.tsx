e86d67e1aeedd38111980af9ec2282aa61359631
Martin <maguera37@gmail.com>
2023-05-10 20:43:43 -0300

    feat: created and added filters component

---
+++ src/UI/components/filters/index.tsx
@@ -0,0 +1,14 @@
+export const Filters: React.FC = () => (
+  <div className="flex justify-between items-center">
+    <div className="flex justify-center items-center gap-2">
+      <label htmlFor="">Range: </label>
+      <input type="range" name="" id="" />
+    </div>
+    <div className="flex justify-center items-center gap-2">
+      <label htmlFor="">Filters: </label>
+      <select name="" id="">
+        <option value="">aaa</option>
+      </select>
+    </div>
+  </div>
+)
--- src/UI/components/index.tsx
+++ src/UI/components/index.tsx
@@ -1,1 +1,2 @@
 export { Title } from './title'
+export { Filters } from './filters'
