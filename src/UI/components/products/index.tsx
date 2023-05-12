08aff245e2eb7b1174a8a8ebded18b7dd1991503
Martin <maguera37@gmail.com>
2023-05-11 21:53:41 -0300

    feat: created products component

---
+++ src/UI/components/products/index.tsx
@@ -0,0 +1,17 @@
+import { useData } from 'hooks/use-data'
+import { Product } from '../product'
+
+export const Products: React.FC = () => {
+  const { data: { products } } = useData()
+
+  return (
+    <ul className="grid grid-cols-auto gap-6">
+      { products.map(product => (
+        <Product
+          key={ product.id }
+          { ...product }
+        />
+      )) }
+    </ul>
+  )
+}
