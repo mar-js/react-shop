a426a0c817d0af02a37a426fc9a706100e3a866a
Martin <maguera37@gmail.com>
2023-05-11 21:49:50 -0300

    feat: created interfaces folder and added interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,30 @@
+export type TCategories = 'all' | 'home-decoration' | 'laptops' | 'smartphones' | 'fragrances' | 'skincare' | 'groceries'
+
+export interface IUseData {
+  data: IData;
+}
+
+export interface IData {
+  products: [] | IProduct[],
+  total: number | null;
+  skip: number | null;
+  limit: number | null;
+}
+
+export interface IProducts {
+  products: IProduct[];
+}
+
+export interface IProduct {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: TCategories;
+  thumbnail: string;
+  images: string[];
+}
