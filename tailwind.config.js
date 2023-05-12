0edbd890eb99813a81240f17fba15633dded37a6
Martin <maguera37@gmail.com>
2023-05-11 21:49:22 -0300

    feat: created new class for gridTemplateColumns

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(250px, 1fr))'
+      }
+    },
   },
   plugins: [],
 }
