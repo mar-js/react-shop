1109038563c69186de341e8a38923f6cc691717f
Martin <maguera37@gmail.com>
2023-05-11 21:51:59 -0300

    feat: added id and styles to the filters

--- src/UI/components/filters/index.tsx
+++ src/UI/components/filters/index.tsx
@@ -1,14 +1,22 @@
-export const Filters: React.FC = () => (
-  <div className="flex justify-between items-center">
-    <div className="flex justify-center items-center gap-2">
-      <label htmlFor="">Range: </label>
-      <input type="range" name="" id="" />
+import { useId } from 'react'
+import { CATEGORIES } from 'utils/constants'
+
+export const Filters: React.FC = () => {
+  const RANGE_ID = useId()
+  const FILTERS_ID = useId()
+
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex justify-center items-center gap-2">
+        <label htmlFor={ RANGE_ID }>Range: </label>
+        <input type="range" name="range" id={ RANGE_ID } />
+      </div>
+      <div className="flex justify-center items-center gap-2">
+        <label htmlFor={ FILTERS_ID }>Filters: </label>
+        <select className="bg-[#1368b8] p-1 capitalize" name="filters" id={ FILTERS_ID }>
+          { CATEGORIES.map(category => <option key={ category } value={ category }>{ category }</option>) }
+        </select>
+      </div>
     </div>
-    <div className="flex justify-center items-center gap-2">
-      <label htmlFor="">Filters: </label>
-      <select name="" id="">
-        <option value="">aaa</option>
-      </select>
-    </div>
-  </div>
-)
+  )
+}
