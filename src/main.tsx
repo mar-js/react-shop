f3ab67dd26d23570d0c8dcc099ee27bce50946fd
Martin <maguera37@gmail.com>
2023-05-22 19:51:07 -0300

    feat: added DataProvider in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { DataProvider } from 'providers/data'
 import { App } from 'App'
-
 import 'styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </React.StrictMode>
 )
