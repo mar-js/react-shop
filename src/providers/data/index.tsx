c7e75a2be4563185cff46d77116d7cddb6f2835f
Martin <maguera37@gmail.com>
2023-05-23 23:28:56 -0300

    feat: added functionnality of filters

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -5,17 +5,40 @@ import {
 } from 'react'
 import { DataModel } from 'contexts/data'
 import DATA from 'mocks/index.json'
-import { INITIAL_DATA } from 'utils/constants'
+import { INITIAL_DATA, INITIAL_FILTERS } from 'utils/constants'
+import {
+  IData,
+  IFilters,
+  IProduct
+} from 'interfaces'
 
 export const DataProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  const [ data, setData ] = useState(INITIAL_DATA)
+  const [ data, setData ] = useState<IData>(INITIAL_DATA)
+  const [ filters, setFilters ] = useState<IFilters>(INITIAL_FILTERS)
 
   useEffect(() => (
     setData(DATA)
   ), [])
 
+  const productsFilter = (): IProduct[] => {
+    const DATA: IProduct[] = data.products.filter(product => (
+      product.price > filters.minPrice || product.category === filters.category
+    ))
+
+    return DATA
+  }
+
+  const VALUE = {
+    data: {
+      ...data,
+      products: productsFilter() || data.products
+    },
+    filters,
+    setFilters
+  }
+
   return (
-    <DataModel.Provider value={ data }>
+    <DataModel.Provider value={ VALUE }>
       { children }
     </DataModel.Provider>
   )
