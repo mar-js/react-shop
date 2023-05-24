cc929ef27886b3f7a4eba667142c1e77ec7f51dc
Martin <maguera37@gmail.com>
2023-05-23 23:30:21 -0300

    feat: created handleChange of range and select for the filters

--- src/UI/components/filters/index.tsx
+++ src/UI/components/filters/index.tsx
@@ -1,19 +1,37 @@
-import { useId } from 'react'
+import { ChangeEvent, useId } from 'react'
+import { useData } from 'contexts/data'
 import { CATEGORIES } from 'utils/constants'
+import { TCategories } from 'interfaces'
 
 export const Filters: React.FC = () => {
+  const { filters, setFilters } = useData()
   const RANGE_ID = useId()
   const FILTERS_ID = useId()
+
+  const handleChangeRange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setFilters(prev => ({
+      ...prev,
+      minPrice: Number(target.value)
+    }))
+  }
+
+  const handleChangeSelect = ({ target }: ChangeEvent<HTMLSelectElement>) => {
+    setFilters(prev => ({
+      ...prev,
+      category: target.value as TCategories
+    }))
+  }
 
   return (
     <div className="flex justify-between items-center">
       <div className="flex justify-center items-center gap-2">
         <label htmlFor={ RANGE_ID }>Range: </label>
-        <input type="range" name="range" id={ RANGE_ID } />
+        <input type="range" name="range" id={ RANGE_ID } value={ filters.minPrice } onChange={ handleChangeRange } />
+        - ${ filters.minPrice }
       </div>
       <div className="flex justify-center items-center gap-2">
         <label htmlFor={ FILTERS_ID }>Filters: </label>
-        <select className="bg-[#1368b8] p-1 capitalize" name="filters" id={ FILTERS_ID }>
+        <select className="bg-[#1368b8] p-1 capitalize" name="filters" id={ FILTERS_ID } value={ filters.category } onChange={ handleChangeSelect }>
           { CATEGORIES.map(category => <option key={ category } value={ category }>{ category }</option>) }
         </select>
       </div>
