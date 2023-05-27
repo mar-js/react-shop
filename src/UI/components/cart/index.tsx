cfb6f6d4b4cc77df3eb0d2323194c5582e4930f7
Martin <maguera37@gmail.com>
2023-05-27 19:51:07 -0300

    feat: created cart component

---
+++ src/UI/components/cart/index.tsx
@@ -0,0 +1,15 @@
+import { useData } from 'contexts/data'
+import { CartIcon } from '../icons'
+
+export const Cart: React.FC = () => {
+  const { carts: { products } } = useData()
+
+  return (
+    <div className="fixed top-5 right-5">
+      <div className="relative rounded-full border-2 p-4">
+        { products.length > 0 && <span className="absolute text-orange-400 text-3xl font-bold top-0 right-2">{ products.length }</span> }
+        <CartIcon />
+      </div>
+    </div>
+  )
+}
