00f30bca7738538a020d4cc4fc2de3cdac12ae75
Martin <maguera37@gmail.com>
2023-05-28 16:51:07 -0300

    feat: added icons in export of the component

--- src/UI/components/index.tsx
+++ src/UI/components/index.tsx
@@ -3,3 +3,6 @@ export { Filters } from './filters'
 export { Products } from './products'
 export { Product } from './product'
 export { Cart } from './cart'
+export {
+  AddToCartIcon, CartIcon, ClearCartIcon, RemoveFromCartIcon
+} from './icons'
