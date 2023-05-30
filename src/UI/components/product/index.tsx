8fd8de47b7b003b8c140aa16adc22fbdb936dd80
Martin <maguera37@gmail.com>
2023-05-30 20:35:36 -0300

    feat: added the button-cart component

--- src/UI/components/product/index.tsx
+++ src/UI/components/product/index.tsx
@@ -1,28 +1,12 @@
-import { useData } from 'contexts/data'
-import { AddToCartIcon, RemoveFromCartIcon } from '../icons'
+import { ButtonCart } from '..'
 import { IProduct } from 'interfaces'
 
-export const Product: React.FC<IProduct> = (product) => {
-  const {
-    checkInCart,
-    addProduct,
-    removeProduct
-  } = useData()
-  const CHECK_IN_CART = checkInCart(product)
-
-  return (
-    <li className="border">
-      <img className="w-full h-52" src={ product.thumbnail } alt={ product.title } />
-      <div className="flex flex-col justify-center items-center my-6 px-2">
-        <h2 className="text-sm mb-2">{ product.title } - ${ product.price }</h2>
-        <button type="button" onClick={ () => CHECK_IN_CART ? removeProduct(product.id) : addProduct(product) }>
-          { CHECK_IN_CART ? (
-            <RemoveFromCartIcon />
-          ) : (
-            <AddToCartIcon />
-          ) }
-        </button>
-      </div>
-    </li>
-  )
-}
+export const Product: React.FC<IProduct> = (product) => (
+  <li className="border">
+    <img className="w-full h-52" src={ product.thumbnail } alt={ product.title } />
+    <div className="flex flex-col justify-center items-center my-6 px-2">
+      <h2 className="text-sm mb-2">{ product.title } - ${ product.price }</h2>
+      <ButtonCart product={ product } />
+    </div>
+  </li>
+)
