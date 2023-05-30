6623a1daa244c8cae7095f4de2b0082655a10ac6
Martin <maguera37@gmail.com>
2023-05-29 23:54:52 -0300

    feat: added cursor-pointer in style and added handleShowAside function

--- src/UI/components/cart/index.tsx
+++ src/UI/components/cart/index.tsx
@@ -2,11 +2,11 @@ import { useData } from 'contexts/data'
 import { CartIcon } from '../icons'
 
 export const Cart: React.FC = () => {
-  const { carts: { products } } = useData()
+  const { carts: { products }, handleShowAside } = useData()
 
   return (
     <div className="fixed top-5 right-5">
-      <div className="relative rounded-full border-2 p-4">
+      <div className="relative rounded-full border-2 p-4 cursor-pointer" onClick={ handleShowAside }>
         { products.length > 0 && <span className="absolute text-orange-400 text-3xl font-bold top-0 right-2">{ products.length }</span> }
         <CartIcon />
       </div>
