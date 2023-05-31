17abff53c393c9e165bb327160cefb5aa87eedae
Martin <maguera37@gmail.com>
2023-05-31 15:51:07 -0300

    feat: added handler of clearFilters

--- src/UI/components/filters/index.tsx
+++ src/UI/components/filters/index.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, useId } from 'react'
 import { useData } from 'contexts/data'
 import { CATEGORIES } from 'utils/constants'
+import { ClearFilters } from '../clear-filters'
 import { TCategories } from 'interfaces'
 
 export const Filters: React.FC = () => {
@@ -22,19 +23,28 @@ export const Filters: React.FC = () => {
     }))
   }
 
+  const conditionShowClearFilters = filters.category !== 'all' || filters.minPrice > 0
+
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex items-center gap-5">
       <div className="flex justify-center items-center gap-2">
         <label htmlFor={ RANGE_ID }>Range: </label>
         <input type="range" name="range" id={ RANGE_ID } value={ filters.minPrice } onChange={ handleChangeRange } />
         - ${ filters.minPrice }
       </div>
+      <b className="text-[#6ee4ff] text-3xl">|</b>
       <div className="flex justify-center items-center gap-2">
         <label htmlFor={ FILTERS_ID }>Filters: </label>
         <select className="bg-[#1368b8] p-1 capitalize" name="filters" id={ FILTERS_ID } value={ filters.category } onChange={ handleChangeSelect }>
           { CATEGORIES.map(category => <option key={ category } value={ category }>{ category }</option>) }
         </select>
       </div>
+      { conditionShowClearFilters && (
+        <>
+          <b className="text-[#6ee4ff] text-3xl">|</b>
+          <ClearFilters />
+        </>
+      ) }
     </div>
   )
 }
